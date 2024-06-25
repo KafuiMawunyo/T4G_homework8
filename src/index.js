caf374d3c6956f12d49d6e531c2438a2240ae800
KafuiMawunyo <siabikafui123@gmail.com>
2024-06-25 07:31:24 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let name1 = prompt('what is your name?');
+let age = prompt('what is your age?');
+let email = prompt('what is your email?');
+alert(`Hi ${name1}, you are ${age} years old and we will keep in touch via your email "${email}"`);
